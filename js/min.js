eb01bbe869b8ff1a2d19ba870518b55f8ee9a1ef
TN-Shadow4K <133524973+Karem-ID@users.noreply.github.com>
2023-11-12 16:06:10 +0100

    started working on the js
    started making rules for the inputs so we can limit what users write on the websie

--- js/min.js
+++ js/min.js
@@ -1,1 +1,16 @@
-const fullName = document.querySelector('.fullName')
+//récupération des valeurs de champs
+const fullName = document.querySelector('.FullName');
+const DateOfBirth = document.querySelector('.DateOfBirth');
+const DateOfBirth = document.querySelector('Select[name="gender"]');
+const task1 = document.querySelector('.task1');
+const task2 = document.querySelector('.task2');
+const task3 = document.querySelector('.task3');
+//Récupérer les bouttons
+const result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
+//validation rules
+function validateFullName(fullName) {
+    //check if the full name is required
+    if
+}
