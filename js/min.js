8feb0709d4983017588bcd0e9c6bf921ab9530fb
asouma <145910922+AsmaBoukadida@users.noreply.github.com>
2023-11-19 14:47:39 +0100

    Updated the min.js file
    updated the min.js  file and added some stuff in there

--- js/min.js
+++ js/min.js
@@ -1,7 +1,7 @@
 //récupération des valeurs de champs
 const fullName = document.querySelector('.FullName');
 const DateOfBirth = document.querySelector('.DateOfBirth');
-const DateOfBirth = document.querySelector('Select[name="gender"]');
+const gender = document.querySelector('Select[name="gender"]');
 const task1 = document.querySelector('.task1');
 const task2 = document.querySelector('.task2');
 const task3 = document.querySelector('.task3');
@@ -12,5 +12,20 @@ const reset = document.querySelector('.reset');
 //validation rules
 function validateFullName(fullName) {
     //check if the full name is required
-    if
+    if (!fullName) {
+        return "Full name is required.";
+    }
+
+    //check the length of full name
+    if(fullName.lenghth < 3 || fullName.lenghth > 20) {
+        return "Full nqme must be between 3 qnd 20 characters long.";
+    }
+    
+    //check rhe format of the full name
+    if (!/^[a-zA-Z ]+$/.test(fullName)) {
+        return "full name must be between 3 and 20 characters long.";
+    }
+
+    //if the full name passes all of the validation rules, return null.
+    return null;
 }
